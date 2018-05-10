e8e5fe15943349de682dc547418bf599092bdcbd
Jed Peek <jedpeek@yahoo.com>
2018-05-10 19:07:00 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,16 +1,17 @@
 def fizzbuzz
   i = 0
-while i < 20
-  if i%3 == 0
-    return "Fizz"
-  elsif i%5 == 0
-    puts "Buzz"
-  elsif i%3 == 0 && i%5 == 0
-    return "FizzBuzz"
-  else
-    return i
-end
-i++
+    while i < 20
+      if i%3 == 0
+        return "Fizz"
+      elsif i%5 == 0
+        puts "Buzz"
+      elsif i%3 == 0 && i%5 == 0
+        return "FizzBuzz"
+      else
+        return i
+    end
+  i++
+  end
 end
 
 fizzbuzz
