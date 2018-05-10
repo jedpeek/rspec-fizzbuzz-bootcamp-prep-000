097c98b3035c913b761d133ce132a3ca1115d8c3
Jed Peek <jedpeek@yahoo.com>
2018-05-10 19:10:40 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,8 @@
 def fizzbuzz(i)
-      if i%3 == 0
+      if i % 3 == 0
         return "Fizz"
       elsif i%5 == 0
-        puts "Buzz"
+        return "Buzz"
       elsif i%3 == 0 && i%5 == 0
         return "FizzBuzz"
       else
